623fce668c8fd39d6bdd564b138f5f4d937df9b8
nithinvisva <visvanic@gmailcom>
2022-08-01 13:31:55 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,26 @@
+const express = require("express")
+const app = express()
+const port = process.env.PORT || 3000
+
+
+app.get("/", (req, res) => {
+  res.send({ message: "Hello World!" })
+})
+const url = `mongodb+srv://nithinvisva:visuakc6999@crudapp.ubtykah.mongodb.net/?retryWrites=true&w=majority`;
+
+const connectionParams={
+    useNewUrlParser: true,
+    // useCreateIndex: true,
+    useUnifiedTopology: true 
+}
+mongoose.connect(url,connectionParams)
+    .then( () => {
+        console.log('Connected to database ')
+    })
+    .catch( (err) => {
+        console.error(`Error connecting to the database. \n${err}`);
+    })
+
+app.listen(port, () => {
+  console.log(`Example app listening at Port: ${port}`)
+})
